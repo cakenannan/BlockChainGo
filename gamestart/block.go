8c46a8e909a121e43ea2f2d58191137002ed19e2
zhujiantao <826674632@qq.com>
2018-06-12 18:29:35 +0800

    a

--- gamestart/block.go
+++ gamestart/block.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"time"
+	"bytes"
+	"encoding/gob"
+	"github.com/labstack/gommon/log"
 )
 
 //定义区块
@@ -27,4 +30,26 @@ func NewBlock(data string, prevHash []byte) *Block {
 //创建创世区块
 func NewGenesisBlock() *Block {
 	return NewBlock("涛酱的创世区块", []byte{})
+}
+
+//对象转为二进制字节集,写入文件
+func (block *Block)Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+	err := encoder.Encode(block)
+	if err != nil {
+		log.Panic(err)
+	}
+	return result.Bytes()
+}
+
+//读取文件,二进制字节集转为对象
+func Deserialize(data []byte) *Block {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+	return &block
 }
